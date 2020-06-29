4b3d526f171db5fefc675180858112804555b570
Jay Oliver <jaykamas@gmail.com>
2020-06-29 19:50:13 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,12 @@
 def find_element_index(array, value_to_find)
-  array.include?(value_to_find)
+  index = 0 
+  while index < array.length do 
+    if array[index] == value_to_find
+      return index
+    end
+    index += 1 
+  end
+  if index == array.length
+    return nil 
+  end
 end
